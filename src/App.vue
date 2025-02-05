63e6607e8dfe1bcd53347e6118367f5a7bc47ee1
Tobias Schmidt <tobias.schmidt@msaid.de>
2025-02-05 12:00:22 +0100

    Now with colored background

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,40 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="app-container">
+    <HelloWorld />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style>
+/* Reset and base styles */
+:root {
+  color-scheme: light;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #ffffff;
+  color: #000000;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.app-container {
+  width: 100%;
+  min-height: 100vh;
+  background-color: #ffffff;
+  color: #000000;
+}
+
+/* Ensure all inputs and textareas have consistent colors */
+input, textarea {
+  background-color: #ffffff;
+  color: #000000;
+  border-color: #cccccc;
+}
+
+/* Ensure text is visible in highlighted sections */
+.highlighted-text {
+  color: #000000;
 }
 </style>
